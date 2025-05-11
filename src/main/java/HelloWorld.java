a558c5dcb34bae3454e237bbd52036f6979e1faf
rizvi <rizvi3389ict@gmail.com>
2025-05-11 15:27:39 +0600

    Java practice - 1

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        List<String> names = Arrays.asList("John", "Jane", "Doe");
+        names.stream();
+    }
+}
